c3e6d2f75cb53d009942ed2cbf7d8f36a0473c04
DVVC1111 <86997642+DVVC1111@users.noreply.github.com>
2023-03-30 13:22:02 +0700

    class-detail

---
+++ class_detail.php
@@ -0,0 +1,46 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// start the session
+session_start();
+
+
+$dbhost = "localhost";
+$dbname = "student_management_system";
+$dbuser = "postgres";
+$dbpass = "David910139";
+
+$conn = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+
+// retrieve the course_id from the query parameter
+$course_id = $_GET['course_id'];
+
+// retrieve details of the selected course from the database
+$stmt = $conn->prepare("SELECT c.course_name, u.username AS professor FROM courses c JOIN users u ON c.id = u.id WHERE c.course_id = :course_id");
+$stmt->bindParam(':course_id', $course_id);
+$stmt->execute();
+$course = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// retrieve details of students enrolled in the selected course from the database
+$stmt = $conn->prepare("SELECT sfname, slname FROM student WHERE course_id = :course_id");
+$stmt->bindParam(':course_id', $course_id);
+$stmt->execute();
+$students = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// store the course and student details in the session
+$_SESSION['courses'] = $course;
+$_SESSION['students'] = $students;
+?>
+
+<!-- display the course and student details -->
+<h2><?= $course['course_name'] ?></h2>
+<p>Professor: <?= $course['professor'] ?></p>
+<h3>Students</h3>
+<ul>
+  <?php foreach ($students as $student): ?>
+    <li><?= $student['sfname'] ?> <?= $student['slname'] ?></li>
+  <?php endforeach; ?>
+</ul>
--- home.php
+++ home.php
@@ -1,3 +1,45 @@
+<?php
+
+session_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$dbhost = "localhost";
+$dbname = "student_management_system";
+$dbuser = "postgres";
+$dbpass = "David910139";
+
+try {
+  // Create a new PDO instance
+  $conn = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+
+  // Set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // Retrieve the courses for the current user
+  $user_id = $_SESSION['user_id'];
+  $stmt = $conn->prepare("SELECT c.course_id, c.course_name, c.num_students, u.username AS professor_name 
+  FROM courses c 
+  JOIN users u ON c.id = u.id 
+  WHERE c.id = :user_id 
+  ORDER BY c.course_id");
+
+  $stmt->bindParam(':user_id', $user_id);
+  $stmt->execute();
+  $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} catch(PDOException $e) {
+  // If an exception is thrown, display an error message
+  echo "Connection failed: " . $e->getMessage();
+}
+
+// Close the database connection
+$conn = null;
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,54 +47,16 @@
 	<link rel="stylesheet" type="text/css" href="home.css">
 </head>
 <body>
-	<div class="container">
-		<h1>Class Dashboard</h1>
-		<?php
-
-            session_start();  
-            $user_id = $_SESSION['user_id'];  
-			// Connect to the database
-			$dbhost = "localhost";
-			$dbname = "student_management_system";
-			$dbuser = "postgres";
-			$dbpass = "David910139";
-
-			try {
-				$conn = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-				// Prepare a SQL query to retrieve class information
-				$stmt = $conn->prepare("SELECT c.course_id, c.course_name, c.num_students, u.username AS professor_name 
-				FROM courses c 
-				JOIN users u ON c.id = u.id 
-				WHERE c.id = :user_id 
-				ORDER BY c.course_id");
-
-				// Bind the user_id parameter to the prepared statement
-				$stmt->bindParam(':user_id', $user_id);
-
-				// Execute the prepared statement
-				$stmt->execute();
-
-				// Fetch the results as an associative array
-				$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-				// Loop through the results and display them as class boxes
-				foreach ($rows as $row) {
-					echo "<div class='class-box'>";
-					echo "<h2>" . $row['course_name'] . "</h2>";
-					echo "<p>Class ID: " . $row['course_id'] . "</p>";
-					echo "<p>Number of Students: " . $row['num_students'] . "</p>";
-					echo "<p>Professor Name: " . $row['professor_name'] . "</p>";
-					echo "</div>";
-				}
-
-				// Close the database connection
-				$conn = null;
-			} catch(PDOException $e) {
-				echo "Connection failed: " . $e->getMessage();
-			}
-		?>
-	</div>
+<h1>Class Dashboard</h1>
+  <?php foreach ($courses as $course): ?>
+    <a href="class_detail.php?course_id=<?php echo $course['course_id']; ?>">
+      <div class="class-box" style="text-decoration:none">
+        <h2><?php echo $course['course_name']; ?></h2>
+        <p>Course ID: <?php echo $course['course_id']; ?></p>
+        <p>Number of Students: <?php echo $course['num_students']; ?></p>
+        <p>Professor: <?php echo $course['professor_name']; ?></p>
+      </div>
+    </a>
+  <?php endforeach; ?>
 </body>
 </html>
